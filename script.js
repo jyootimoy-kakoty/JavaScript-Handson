101e5bad05628e43c8d2d2c240b80f4daa16b1f7
jyootimoy-kakoty <jyootimoy.kakoty@gmail.com>
2020-09-05 21:20:10 +0530

    use of alert

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello JavaScript!');
+
+var firstName = "Jyootimoy";
+var lastName = "Kakoty";
+
+console.log("Hello!! " + firstName + " " + lastName);
+
+var age = prompt('What is his age?');
+console.log(age);
+
+alert('let\'s go ahead!! :)');
